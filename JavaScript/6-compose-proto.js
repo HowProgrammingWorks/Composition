1c82731cb382e1148f98bfcf568bc7337aaf5b8c
tshemsedinov <timur.shemsedinov@gmail.com>
2017-09-04 04:30:31 +0300

    Add OOP composition

---
+++ JavaScript/6-compose-proto.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { EventEmitter } = require('events');
+
+function AddOnlySet(it) {
+  this.set = new Set(it);
+  this.emitter = new EventEmitter();
+  this.size = it.length;
+}
+
+AddOnlySet.prototype.add = function(value) {
+  this.set.add(value);
+  this.size = this.set.size;
+  this.emitter.emit('add', value);
+};
+
+AddOnlySet.prototype.on = function(name, listener) {
+  this.emitter.on(name, listener);
+};
+
+AddOnlySet.prototype.toString = function() {
+  return [...this.set.values()].join();
+};
+
+const s1 = new AddOnlySet(['uno', 'due']);
+s1.on('add', value => console.log(`Added "${value}"`));
+s1.add('tre');
+console.dir({ result: s1.toString() });
