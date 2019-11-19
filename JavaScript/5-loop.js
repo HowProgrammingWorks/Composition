2786e8f786b04ea1d3e452ef17bbc1df7ce6cb68
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2019-11-19 20:46:00 +0200

    Optimize implementation

--- JavaScript/5-loop.js
+++ JavaScript/5-loop.js
@@ -2,10 +2,9 @@
 
 const compose = (...fns) => x => {
   if (fns.length === 0) return x;
-
   const last = fns.length - 1;
-  let res = fns[last](x);
-  for (let i = last - 1; i >= 0; i--) {
+  let res = x;
+  for (let i = last; i >= 0; i--) {
     res = fns[i](res);
   }
   return res;
@@ -13,9 +12,8 @@ const compose = (...fns) => x => {
 
 const pipe = (...fns) => x => {
   if (fns.length === 0) return x;
-
-  let res = fns[0](x);
-  for (let i = 1; i < fns.length; i++) {
+  let res = x;
+  for (let i = 0; i < fns.length; i++) {
     res = fns[i](res);
   }
   return res;
