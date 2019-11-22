cc4c2c9736f86d192bdc3d9ecb806e2125abd8e3
ser <ser@localhost.localdomain>
2019-11-22 08:27:16 +0200

    work, but some tests failed

--- Exercises/2-compose.js
+++ Exercises/2-compose.js
@@ -1,5 +1,28 @@
 'use strict';
 
-const compose = (...fns) => x => null;
+const compose = (...fns) => {
+  const events = {};
+  const fn = num => {
+    return fns.reverse().reduce((val, fn) => {
+      try {
+        return fn(val);
+      } catch (e) {
+        fn.emmit('error', e)
+        return undefined;
+      }
+    }, num);
+  };
+  fn.on = (event, callback) => {
+    let res = events[event];
+    if (!res) res = [];
+    res.push(callback);
+    events[event] = res;
+  }
+  fn.emmit = (event, ...args) => {
+    const res = events[event];
+    if (res) res.forEach(callback => callback(...args));
+  }
+  return fn;
+};
 
 module.exports = { compose };
