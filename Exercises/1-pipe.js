a362f1f43c9dd7ed83170d0df666ddc81ca6067d
gidra5 <hranitel.52@gmail.com>
2019-11-24 19:09:26 +0200

    Tasks done

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const pipe = (...fns) => x => null;
+const pipe = (...fns) => {
+  if (fns.reduce((x, f) => typeof f !== 'function' | x, false)) {
+    throw new Error('Not a function');
+  }
+
+  return x => fns.reduce((x, f) => f(x), x);
+};
 
 module.exports = { pipe };
--- Exercises/2-compose.js
+++ Exercises/2-compose.js
@@ -1,5 +1,30 @@
 'use strict';
 
-const compose = (...fns) => x => null;
+const compose = (...fns) => {
+  const events = { };
+
+  const emit = (event, ...args) => {
+    events[event](args);
+  };
+
+  const f = function (x) {
+    let y = x;
+    try {
+      for (const f of fns.reverse()) {
+        y = f(y);
+      }
+    } catch (e) {
+      emit('error', e);
+      return undefined;
+    }
+    return y;
+  };
+
+  f.on = (event, callback) => {
+    events[event] = callback;
+  };
+
+  return f;
+};
 
 module.exports = { compose };
