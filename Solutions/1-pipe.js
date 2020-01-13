587e94c88d5a4e354e572d9ab28d404f095461d8
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-01-14 01:22:15 +0200

    Add solutions

---
+++ Solutions/1-pipe.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const pipe = (...fns) => {
+  for (const f of fns) {
+    if (typeof f !== 'function') {
+      throw new Error('All compose arguments should be functions');
+    }
+  }
+  return x => fns.reduce((v, f) => f(v), x);
+};
+
+module.exports = { pipe };
---
+++ Solutions/2-compose.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const compose = (...fns) => {
+  const handlers = [];
+  const composed = x => {
+    if (fns.length === 0) return x;
+    const last = fns.length - 1;
+    let res = x;
+    try {
+      for (let i = last; i >= 0; i--) {
+        res = fns[i](res);
+      }
+      return res;
+    } catch (error) {
+      for (const handler of handlers) {
+        handler(error);
+      }
+    }
+  };
+  composed.on = (name, handler) => {
+    if (name === 'error') handlers.push(handler);
+  };
+  return composed;
+};
+
+module.exports = { compose };
