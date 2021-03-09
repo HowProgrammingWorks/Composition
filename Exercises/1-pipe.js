9305d2768cba9d7d575f3b7f7197ef98d1740eca
beliy-a <b.a.v.beliy@gmail.com>
2021-03-09 20:43:36 +0300

    add solving for all tasks

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const pipe = (...fns) => x => null;
+const pipe = (...fns) => {
+  fns.forEach(fn => {
+    if (typeof fn !== 'function') throw Error('not a Function');
+  });
+
+  return x => fns.reduce((acc, fn) => acc = fn(acc), x);
+};
+
 
 module.exports = { pipe };
--- Exercises/2-compose.js
+++ Exercises/2-compose.js
@@ -1,5 +1,29 @@
 'use strict';
 
-const compose = (...fns) => x => null;
+const compose = (...fns) => {
+  const errors = [];
+
+  const fn = x => {
+    if (fns.length === 0) return x;
+
+    const start = fns.length - 1;
+    let res = x;
+
+    try {
+      for (let i = start; i >= 0; i--) {
+        res = fns[i](res);
+      }
+      return res;
+    } catch (err) {
+      errors.forEach(e => e(err));
+    }
+  };
+
+  fn.on = callback => {
+    errors.forEach(err => callback(err));
+  };
+
+  return fn;
+};
 
 module.exports = { compose };
