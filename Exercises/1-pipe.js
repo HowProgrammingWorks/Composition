2d4731d5b4861dd538f57295381d12e2a34e30f8
AIDXYLELELE <warboyka48@gmail.com>
2021-12-28 20:58:34 +0200

    Lab_by_Aid

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,14 @@
 'use strict';
 
-const pipe = (...fns) => x => null;
+const pipe = (...fns) => {
+    for (const funct of fns) {
+        if (typeof funct !== 'function') {
+            throw new Error('It is not a function ');
+        } else {
+            return x => fns.reduce((v, f) => f(v), x);
+        }
+    }
+
+};
 
 module.exports = { pipe };
--- Exercises/2-compose.js
+++ Exercises/2-compose.js
@@ -1,5 +1,26 @@
 'use strict';
 
-const compose = (...fns) => x => null;
+const compose = (...fns) => {
+    const errs = [];
+    const composed = x => {
+        if (fns.length === 0) return x;
+        const last = fns.length;
+        let res = x;
+        try {
+            for (let i = last - 1; i >= 0; i--) {
+                res = fns[i](res);
+            }
+            return res;
+        } catch (error) {
+            for (const err of errs) {
+                err(error);
+            }
+        }
+    };
+    composed.on = (name, err) => {
+        if (name === 'error') errs.push(err);
+    };
+    return composed;
+};
 
 module.exports = { compose };
