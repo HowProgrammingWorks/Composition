46de60d04e4b3b34cca2c4d20e41576fb9354833
YanPetrov7 <yanemerald2004@gmail.com>
2022-01-29 13:23:15 +0200

    pipe, compose

--- Exercises/2-compose.js
+++ Exercises/2-compose.js
@@ -1,5 +1,24 @@
 'use strict';
 
-const compose = (...fns) => x => null;
+const compose = (...fns) => x => {
+  for (const i in fns) {
+    if (typeof fns[i] !== 'function') {
+      throw undefined;
+    }
+  }
+  let res = x;
+  const last = fns.length - 1;
+  for (let i = last; i >= 0; i--) {
+    res = fns[i](res);
+  }
+  return res;
+};
+
+const twice = x => x * 2;
+const cube = x => x * 3;
+
+const capitalize = compose(1, cube, twice);
+console.log(`use capitalize = '${capitalize(10)}'`);
+
 
 module.exports = { compose };
