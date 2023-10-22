9b8b0645222080685d0f4f4aba014003c98b4072
Jhon Mosk <jhon-mosk@yandex.ru>
2023-10-22 15:36:44 +0300

    All tasks was done

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const pipe = (...fns) => (x) => null;
+const pipe = (...fns) => {
+  fns.forEach((fn) => {
+    if (typeof fn !== 'function') {
+      throw new Error('Type of fn is not a function');
+    }
+  });
+  return (x) => fns.reduce((v, f) => f(v), x);
+};
 
 module.exports = { pipe };
